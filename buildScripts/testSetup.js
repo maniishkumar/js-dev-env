946954c4058816bd58b82df50ec5c21cad8ed0b9
Manish Kumar <maniish.kumar@gmail.com>
2017-12-13 15:10:27 +0530

    completed work

--- buildScripts/testSetup.js
+++ buildScripts/testSetup.js
@@ -1,7 +1,7 @@
-// This file isn't transpiled, so must use commonJS and ES5
+// This file isn't transpiled, so must use CommonJS and ES5
 
-//Register babel to transpile before test run
-require('babel-register');
+// Register babel to transpile before our tests run.
+require('babel-register')();
 
-//Disable webpack feature that Mocha doesn't understand
-require.extensions['.css'] = function(){};
+// Disable webpack features that Mocha doesn't understand.
+require.extensions['.css'] = function() {};
